ef89edc75cbe84214ae7d91e7f22908e70dc41d2
bonfy <foreverbonfy@163.com>
2016-02-18 09:30:42 +0800

    add config

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+
+# default config
+class BaseConfig(object):
+    DEBUG = False
+    # shortened for readability
+    SECRET_KEY = '\xbf\xb0\x11\xb1\xcd\xf9\xba\x8bp\x0c...'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://sa:@127.0.0.1:1433/flask'
+
+    # SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost:3306/flask?charset=utf8'
+
+    print SQLALCHEMY_DATABASE_URI
+
+
+class TestConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://sa:@127.0.0.1:1433/flask_test'
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
