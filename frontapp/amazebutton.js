d505fb8f13e1ba300d41cd7ef6618e6c9eab5c67
bonfy <foreverbonfy@163.com>
2016-03-10 11:08:22 +0800

    complete React with ajax

--- frontapp/amazebutton.js
+++ frontapp/amazebutton.js
@@ -1,15 +1,73 @@
 var React = require('react');
+
 var AMUIReact = require('amazeui-react');
+var reqwest = require('reqwest');
 var Button = AMUIReact.Button;
 
-var buttons  = React.createClass({
+
+var ProbList = React.createClass({
+  render: function(){
+
+    var Nodes = this.props.data.map(function (node){
+                  return (
+                      <div key={node.id}>ID: {node.id}  PBC: {node.PBC}</div>
+                  );
+              });
+
+    return (
+          <div className="probList">
+              {Nodes}
+          </div>
+      );
+  }
+
+});
+
+
+var MainBoard  = React.createClass({
+
+    getInitialState: function() {
+        return {clicktimes: 0 ,data : []};
+     },
+/*
+    componentDidMount: function() {
+       reqwest({
+            url: 'http://127.0.0.1:5000/api/prob'
+          , crossOrigin: true
+        }, function (resp) {
+          if (this.isMounted()) {
+            this.setState({
+              data: resp.data,
+              clicktimes: 20
+            });
+          }
+        }.bind(this));
+  },
+*/
+    handleClick: function(e) {
+        e.preventDefault();
+        console.log('click times:' + this.state.clicktimes);
+        this.setState({clicktimes: this.state.clicktimes + 1});
+        reqwest({
+            url: 'http://127.0.0.1:5000/api/prob'
+          , crossOrigin: true
+        }, function (resp) {
+            this.setState({
+              data: resp.data,
+            });
+        }.bind(this));
+    },
+
     render: function(){
+      
       return (
-           <div> <Button amStyle="secondary">Default</Button>
-           <Button amStyle="primary">dds</Button></div>
+           <div> 
+           <Button onClick={this.handleClick}>Default</Button>
+           <ProbList data={this.state.data}/>
+           </div>
           );
     }
 
 });
 
-module.exports = buttons;
+module.exports = MainBoard;
